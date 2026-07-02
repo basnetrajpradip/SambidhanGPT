e42fe9988fc8962c0a5159dc974b1f8677d6dadd
Neeraj319 <itskop520@gmail.com>
2026-07-02 10:49:29 +0545

    fix: multer issue, folder not found

--- backend/configs/multer.ts
+++ backend/configs/multer.ts
@@ -1,9 +1,14 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
+
+const uploadDirectory = path.join(__dirname, '../uploads/pdfs');
+
+fs.mkdirSync(uploadDirectory, { recursive: true });
 
 const storage = multer.diskStorage({
 	destination: function (_req, _file, cb) {
-		cb(null, path.join(__dirname, '../uploads/pdfs'));
+		cb(null, uploadDirectory);
 	},
 	filename: function (_req, file, cb) {
 		cb(null, `${Date.now()}-${file.originalname}`);
